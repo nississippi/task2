082d7752bc5bf2f8baea38e24207c5f4aa7272b8
EC2 Default User <ec2-user@ip-172-31-16-143.ap-northeast-1.compute.internal>
2022-07-09 15:04:40 +0000

    [update]4-12ルートパス設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  get '/' => 'homes#top'
+  root :to => 'homes#top'
   resources :books, except: [:new]
   get 'books' => 'books#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

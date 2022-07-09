478ece8338d91234ebcdee86efd3214039f8db07
EC2 Default User <ec2-user@ip-172-31-16-143.ap-northeast-1.compute.internal>
2022-07-09 09:26:57 +0000

    [add]4-12index=>showリンク変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,31 @@
 class BooksController < ApplicationController
   def index
+    #表示するためのインスタンス変数に空のModelオブジェクトを作成
+    #自動的にcreateアクションのURLへ送信できる
     @book = Book.new
+    #テーブルに保存されたデータを全て取得
     @books = Book.all
   end
   
   def create
+    #入力されたデータを受け取りローカル変数に格納
+    #ビューに渡す必要がないので@はつけない
     book = Book.new(book_params)
+    #呼び出したModelインスタンスをデータベースに保存するメソッド
     book.save
     redirect_to '/books/:id'
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
   
+  #privateより下の記述はアクションとして認識されないのでURLと対応できなくなる
   private
+  #フォームから送られてくるデータはparamsに入っており、requireでモデル名を指定、permitで保存を許可するカラムを指定
   def book_params
     params.require(:book).permit(:title, :body)
   end
